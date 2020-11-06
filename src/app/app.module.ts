485048b5c6a31518659d2b943988340e9880a352
mbagiella <mbagiella@gmail.com>
2020-11-06 12:25:17 +0100

    angular updata

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from "@angular/common/http"; 
 import { AppComponent } from './app.component';
 import localeFr from '@angular/common/locales/fr'
@@ -15,7 +14,6 @@ registerLocaleData(localeFr);
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot(),
     HttpClientModule,
     BrowserAnimationsModule
   ],
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -19,10 +19,10 @@
 */
 
 
-/** IE10 and IE11 requires the following for NgClass support on SVG elements */
+/** IE10 and IE11 requires the following for NgClass support on SVG elements 
  import 'classlist.js';  // Run `npm install --save classlist.js`.
 
-/** IE10 and IE11 requires the following for the Reflect API. */
+/** IE10 and IE11 requires the following for the Reflect API. 
  import 'core-js/es6/reflect';
 
 
